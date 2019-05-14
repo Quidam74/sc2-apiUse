4dfbb3fca45f5307f37ca5ad032981bdb627e125
Unknown <toutlemonde74@gmail.com>
2019-05-14 16:17:28 +0200

    basic get data

--- src/js/main.js
+++ src/js/main.js
@@ -11,9 +11,34 @@ const App = {
     ready(){
         this.initComponents();
         this.bindEvent();
-        console.log("aaa")
-        //console.log(getBrowser());
-        //new Page();
+
+        fetch('https://eu.api.blizzard.com/sc2/profile/2/1/1577911?locale=fr_FR&access_token=')
+        .then(function(response) {
+          response.json().then(function(gameProfile){
+           //console.log(gameProfile);
+           document.querySelector(".hero-name").innerHTML = gameProfile.summary.displayName
+           document.querySelector(".hero-TotalSucces").innerHTML = gameProfile.summary.totalAchievementPoints
+           document.querySelector(".hero-levels-total").innerHTML = gameProfile.summary.totalSwarmLevel
+           document.querySelector(".hero-levels-zerg").innerHTML = gameProfile.swarmLevels.zerg.level
+           document.querySelector(".hero-levels-terran").innerHTML = gameProfile.swarmLevels.terran.level
+           document.querySelector(".hero-levels-protoss").innerHTML = gameProfile.swarmLevels.protoss.level
+
+           document.querySelector(".hero-career-max").innerHTML = gameProfile.career.best1v1Finish.leagueName
+           document.querySelector(".hero-career-maxTime").innerHTML = gameProfile.career.best1v1Finish.timesAchieved
+
+           document.querySelector(".hero-careerTeam-max").innerHTML = gameProfile.career.bestTeamFinish.leagueName
+           document.querySelector(".hero-careerTeam-maxTime").innerHTML = gameProfile.career.bestTeamFinish.timesAchieved
+       })
+      });
+
+
+ fetch('https://eu.api.blizzard.com/sc2/profile/2/1/1577911/ladder/summary?locale=fr_FR&access_token=')
+        .then(function(response) {
+          response.json().then(function(gameLader){
+           console.log(gameLader);
+            })
+      });
+
     },
 
     bindEvent(){
@@ -23,8 +48,9 @@ const App = {
     initComponents(){
 
 
-    }
+    },
+
 };
 
- 
+
 App.init();
--- web/js/main.js
+++ web/js/main.js
@@ -9,8 +9,26 @@ var App = {
   ready: function ready() {
     this.initComponents();
     this.bindEvent();
-    console.log("aaa"); //console.log(getBrowser());
-    //new Page();
+    fetch('https://eu.api.blizzard.com/sc2/profile/2/1/1577911?locale=fr_FR&access_token=').then(function (response) {
+      response.json().then(function (gameProfile) {
+        //console.log(gameProfile);
+        document.querySelector(".hero-name").innerHTML = gameProfile.summary.displayName;
+        document.querySelector(".hero-TotalSucces").innerHTML = gameProfile.summary.totalAchievementPoints;
+        document.querySelector(".hero-levels-total").innerHTML = gameProfile.summary.totalSwarmLevel;
+        document.querySelector(".hero-levels-zerg").innerHTML = gameProfile.swarmLevels.zerg.level;
+        document.querySelector(".hero-levels-terran").innerHTML = gameProfile.swarmLevels.terran.level;
+        document.querySelector(".hero-levels-protoss").innerHTML = gameProfile.swarmLevels.protoss.level;
+        document.querySelector(".hero-career-max").innerHTML = gameProfile.career.best1v1Finish.leagueName;
+        document.querySelector(".hero-career-maxTime").innerHTML = gameProfile.career.best1v1Finish.timesAchieved;
+        document.querySelector(".hero-careerTeam-max").innerHTML = gameProfile.career.bestTeamFinish.leagueName;
+        document.querySelector(".hero-careerTeam-maxTime").innerHTML = gameProfile.career.bestTeamFinish.timesAchieved;
+      });
+    });
+    fetch('https://eu.api.blizzard.com/sc2/profile/2/1/1577911/ladder/summary?locale=fr_FR&access_token=').then(function (response) {
+      response.json().then(function (gameLader) {
+        console.log(gameLader);
+      });
+    });
   },
   bindEvent: function bindEvent() {},
   initComponents: function initComponents() {}
