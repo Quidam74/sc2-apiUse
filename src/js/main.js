1a0bb43c91316034f8f6b1acd3fc062331414a7d
Unknown <toutlemonde74@gmail.com>
2019-06-09 16:37:50 +0200

    js crade mais ok ! niveau sécurité

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 //import Page from './components/Page'
 //import {getBrowser} from './utils/environment'
 
+var token = 'aaa'
 const App = {
 
   init () {
@@ -8,87 +9,92 @@ const App = {
   },
   ready () {
     this.initComponents();
+
     this.bindEvent();
-    var that = this;
-    var mapProgress = [];
-    var recencedMap = [];
-    fetch('https://eu.api.blizzard.com/sc2/profile/2/1/1577911?locale=fr_FR&access_token=')
-        .then(function (response) {
-          response.json().then(function (gameProfile) {
-            //console.log(gameProfile);
-            document.querySelector(".hero-name").innerHTML = gameProfile.summary.displayName
-            document.querySelector(".hero-TotalSucces").innerHTML = gameProfile.summary.totalAchievementPoints
-            document.querySelector(".hero-levels-total").innerHTML = gameProfile.summary.totalSwarmLevel
-            document.querySelector(".hero-levels-zerg").innerHTML = gameProfile.swarmLevels.zerg.level
-            document.querySelector(".hero-levels-terran").innerHTML = gameProfile.swarmLevels.terran.level
-            document.querySelector(".hero-levels-protoss").innerHTML = gameProfile.swarmLevels.protoss.level
+  },
+  bindEvent () {
+  },
+  getToken () {
+    var that = this
+    fetch("https://us.battle.net/oauth/token", {
+      body: "grant_type=client_credentials",
+      headers: {
+        Authorization: "Basic NzFhMTY0ZmMwNDc0NDg4Y2IyYWM4ZDJhZDJkY2MyYjE6dmdlZVNpczdGTjFMMUVYNGZjekJ6OTZ5U21xRGo5RE4=",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      method: "POST"
+    }).then(function (response) {
+      response.json().then(function (tokenData) {
+        token = tokenData.access_token
+        var mapProgress = [];
+        var recencedMap = [];
+        fetch('https://eu.api.blizzard.com/sc2/profile/2/1/1577911?locale=fr_FR&access_token=' + token)
+            .then(function (response) {
+              response.json().then(function (gameProfile) {
+                //console.log(gameProfile);
+                document.querySelector(".hero-name").innerHTML = gameProfile.summary.displayName
+                document.querySelector(".hero-TotalSucces").innerHTML = gameProfile.summary.totalAchievementPoints
+                document.querySelector(".hero-levels-total").innerHTML = gameProfile.summary.totalSwarmLevel
+                document.querySelector(".hero-levels-zerg").innerHTML = gameProfile.swarmLevels.zerg.level
+                document.querySelector(".hero-levels-terran").innerHTML = gameProfile.swarmLevels.terran.level
+                document.querySelector(".hero-levels-protoss").innerHTML = gameProfile.swarmLevels.protoss.level
 
-            document.querySelector(".hero-career-max").innerHTML = gameProfile.career.best1v1Finish.leagueName
-            document.querySelector(".hero-career-maxTime").innerHTML = gameProfile.career.best1v1Finish.timesAchieved
+                document.querySelector(".hero-career-max").innerHTML = gameProfile.career.best1v1Finish.leagueName
+                document.querySelector(".hero-career-maxTime").innerHTML = gameProfile.career.best1v1Finish.timesAchieved
 
-            document.querySelector(".hero-careerTeam-max").innerHTML = gameProfile.career.bestTeamFinish.leagueName
-            document.querySelector(".hero-careerTeam-maxTime").innerHTML = gameProfile.career.bestTeamFinish.timesAchieved
-          })
-              .catch(function (error) {
-                console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message + "c'est sans doute lié a une clé d'api mal renseigné !");
-              });
-        });
+                document.querySelector(".hero-careerTeam-max").innerHTML = gameProfile.career.bestTeamFinish.leagueName
+                document.querySelector(".hero-careerTeam-maxTime").innerHTML = gameProfile.career.bestTeamFinish.timesAchieved
+              })
+                  .catch(function (error) {
+                    console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message + "c'est sans doute lié a une clé d'api mal renseigné !");
+                  });
+            });
 
-    fetch('https://eu.api.blizzard.com/sc2/legacy/profile/2/1/1577911/matches?access_token=')
-        .then(function (response) {
-          response.json().then(function (gameLader) {
-
-            gameLader.matches.forEach(function (elem, index) {
-              var mapIsCounted = false;
-              var formattedTime = that.casttimestampToDate(elem.date);
-              document.querySelector(".history").innerHTML += "<div><span>" + elem.map + "</span><span>" + elem.type + "</span><span>" + formattedTime + "</span><span>" + elem.decision + "</span></div>";
-              if (recencedMap.length == 0) {
-                recencedMap.push(elem.map)
-                that.pushFirstMap(mapProgress, elem.decision, elem.map)
-              } else {
-                recencedMap.forEach(function (mapName, position) {
-                  if (mapName == elem.map) {
-                    mapIsCounted = true;
+        fetch('https://eu.api.blizzard.com/sc2/legacy/profile/2/1/1577911/matches?access_token=' + token)
+            .then(function (response) {
+              response.json().then(function (gameLader) {
+                gameLader.matches.forEach(function (elem, index) {
+                  var mapIsCounted = false;
+                  var formattedTime = that.casttimestampToDate(elem.date);
+                  document.querySelector(".history").innerHTML += "<div><span>" + elem.map + "</span><span>" + elem.type + "</span><span>" + formattedTime + "</span><span>" + elem.decision + "</span></div>";
+                  if (recencedMap.length == 0) {
+                    recencedMap.push(elem.map)
+                    that.pushFirstMap(mapProgress, elem.decision, elem.map)
+                  } else {
+                    recencedMap.forEach(function (mapName, position) {
+                      if (mapName == elem.map) {
+                        mapIsCounted = true;
+                      }
+                    })
+                    if (!mapIsCounted) {
+                      recencedMap.push(elem.map)
+                      that.pushFirstMap(mapProgress, elem.decision, elem.map)
+                    } else {
+                      mapProgress.forEach(function (stats, nombre) {
+                        if (stats.mapName == elem.map) {
+                          if (elem.decision == "Win")
+                            stats.win += 1;
+                          else
+                            stats.lose += 1;
+                        }
+                      })
+                    }
                   }
                 })
-
-                if (!mapIsCounted) {
-                  recencedMap.push(elem.map)
-                  that.pushFirstMap(mapProgress, elem.decision, elem.map)
-                } else {
-                  mapProgress.forEach(function (stats, nombre) {
-                    if (stats.mapName == elem.map) {
-                      if (elem.decision == "Win")
-                        stats.win += 1;
-                      else
-                        stats.lose += 1;
-                    }
-                  })
-                }
-              }
-            })
-            mapProgress.forEach(function (stats, nombre) {
-              stats.ratio = Math.round(eval(stats.win / stats.lose) * 100) / 100
-            })
-            mapProgress = that.sortByKey(mapProgress, "ratio")
-            mapProgress.forEach(function (stats, nombre) {
-              document.querySelector(".mapAnalyse").innerHTML += "<div><span>" + stats.mapName + "</span><span>" + stats.ratio + "</span></div>";
-            })
-
-          })
-
-
-        });
-
+                mapProgress.forEach(function (stats, nombre) {
+                  stats.ratio = Math.round(eval(stats.win / stats.lose) * 100) / 100
+                })
+                mapProgress = that.sortByKey(mapProgress, "ratio")
+                mapProgress.forEach(function (stats, nombre) {
+                  document.querySelector(".mapAnalyse").innerHTML += "<div><span>" + stats.mapName + "</span><span>" + stats.ratio + "</span></div>";
+                })
+              })
+            });
+      })
+    })
   },
-
-  bindEvent () {
-
-  },
-
   initComponents () {
-
-
+    this.getToken()
   },
   casttimestampToDate (timeStamp) {
     var date = new Date(timeStamp * 1000);
@@ -101,13 +107,15 @@ const App = {
     var year = +date.getUTCFullYear();
     var formattedTime = day + '/' + month + '/' + year;
     return formattedTime;
-  },
+  }
+  ,
   pushFirstMap (mapProgress, decision, map) {
     if (decision == "Win")
       mapProgress.push({"mapName": map, "win": 1, "lose": 0})
     else
       mapProgress.push({"mapName": map, "win": 0, "lose": 1})
-  },
+  }
+  ,
   sortByKey (array, key) {
     return array.sort(function (a, b) {
       var x = a[key];
--- web/js/main.js
+++ web/js/main.js
@@ -2,6 +2,7 @@
 
 //import Page from './components/Page'
 //import {getBrowser} from './utils/environment'
+var token = 'aaa';
 var App = {
   init: function init() {
     document.addEventListener('DOMContentLoaded', this.ready.bind(this), false);
@@ -9,67 +10,83 @@ var App = {
   ready: function ready() {
     this.initComponents();
     this.bindEvent();
+  },
+  bindEvent: function bindEvent() {},
+  getToken: function getToken() {
     var that = this;
-    var mapProgress = [];
-    var recencedMap = [];
-    fetch('https://eu.api.blizzard.com/sc2/profile/2/1/1577911?locale=fr_FR&access_token=').then(function (response) {
-      response.json().then(function (gameProfile) {
-        //console.log(gameProfile);
-        document.querySelector(".hero-name").innerHTML = gameProfile.summary.displayName;
-        document.querySelector(".hero-TotalSucces").innerHTML = gameProfile.summary.totalAchievementPoints;
-        document.querySelector(".hero-levels-total").innerHTML = gameProfile.summary.totalSwarmLevel;
-        document.querySelector(".hero-levels-zerg").innerHTML = gameProfile.swarmLevels.zerg.level;
-        document.querySelector(".hero-levels-terran").innerHTML = gameProfile.swarmLevels.terran.level;
-        document.querySelector(".hero-levels-protoss").innerHTML = gameProfile.swarmLevels.protoss.level;
-        document.querySelector(".hero-career-max").innerHTML = gameProfile.career.best1v1Finish.leagueName;
-        document.querySelector(".hero-career-maxTime").innerHTML = gameProfile.career.best1v1Finish.timesAchieved;
-        document.querySelector(".hero-careerTeam-max").innerHTML = gameProfile.career.bestTeamFinish.leagueName;
-        document.querySelector(".hero-careerTeam-maxTime").innerHTML = gameProfile.career.bestTeamFinish.timesAchieved;
-      })["catch"](function (error) {
-        console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message + "c'est sans doute lié a une clé d'api mal renseigné !");
-      });
-    });
-    fetch('https://eu.api.blizzard.com/sc2/legacy/profile/2/1/1577911/matches?access_token=').then(function (response) {
-      response.json().then(function (gameLader) {
-        gameLader.matches.forEach(function (elem, index) {
-          var mapIsCounted = false;
-          var formattedTime = that.casttimestampToDate(elem.date);
-          document.querySelector(".history").innerHTML += "<div><span>" + elem.map + "</span><span>" + elem.type + "</span><span>" + formattedTime + "</span><span>" + elem.decision + "</span></div>";
+    fetch("https://us.battle.net/oauth/token", {
+      body: "grant_type=client_credentials",
+      headers: {
+        Authorization: "Basic NzFhMTY0ZmMwNDc0NDg4Y2IyYWM4ZDJhZDJkY2MyYjE6dmdlZVNpczdGTjFMMUVYNGZjekJ6OTZ5U21xRGo5RE4=",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      method: "POST"
+    }).then(function (response) {
+      response.json().then(function (tokenData) {
+        token = tokenData.access_token;
+        var mapProgress = [];
+        var recencedMap = [];
+        fetch('https://eu.api.blizzard.com/sc2/profile/2/1/1577911?locale=fr_FR&access_token=' + token).then(function (response) {
+          response.json().then(function (gameProfile) {
+            //console.log(gameProfile);
+            document.querySelector(".hero-name").innerHTML = gameProfile.summary.displayName;
+            document.querySelector(".hero-TotalSucces").innerHTML = gameProfile.summary.totalAchievementPoints;
+            document.querySelector(".hero-levels-total").innerHTML = gameProfile.summary.totalSwarmLevel;
+            document.querySelector(".hero-levels-zerg").innerHTML = gameProfile.swarmLevels.zerg.level;
+            document.querySelector(".hero-levels-terran").innerHTML = gameProfile.swarmLevels.terran.level;
+            document.querySelector(".hero-levels-protoss").innerHTML = gameProfile.swarmLevels.protoss.level;
+            document.querySelector(".hero-career-max").innerHTML = gameProfile.career.best1v1Finish.leagueName;
+            document.querySelector(".hero-career-maxTime").innerHTML = gameProfile.career.best1v1Finish.timesAchieved;
+            document.querySelector(".hero-careerTeam-max").innerHTML = gameProfile.career.bestTeamFinish.leagueName;
+            document.querySelector(".hero-careerTeam-maxTime").innerHTML = gameProfile.career.bestTeamFinish.timesAchieved;
+          })["catch"](function (error) {
+            console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message + "c'est sans doute lié a une clé d'api mal renseigné !");
+          });
+        });
+        fetch('https://eu.api.blizzard.com/sc2/legacy/profile/2/1/1577911/matches?access_token=' + token).then(function (response) {
+          response.json().then(function (gameLader) {
+            gameLader.matches.forEach(function (elem, index) {
+              var mapIsCounted = false;
+              var formattedTime = that.casttimestampToDate(elem.date);
+              document.querySelector(".history").innerHTML += "<div><span>" + elem.map + "</span><span>" + elem.type + "</span><span>" + formattedTime + "</span><span>" + elem.decision + "</span></div>";
 
-          if (recencedMap.length == 0) {
-            recencedMap.push(elem.map);
-            that.pushFirstMap(mapProgress, elem.decision, elem.map);
-          } else {
-            recencedMap.forEach(function (mapName, position) {
-              if (mapName == elem.map) {
-                mapIsCounted = true;
+              if (recencedMap.length == 0) {
+                recencedMap.push(elem.map);
+                that.pushFirstMap(mapProgress, elem.decision, elem.map);
+              } else {
+                recencedMap.forEach(function (mapName, position) {
+                  if (mapName == elem.map) {
+                    mapIsCounted = true;
+                  }
+                });
+
+                if (!mapIsCounted) {
+                  recencedMap.push(elem.map);
+                  that.pushFirstMap(mapProgress, elem.decision, elem.map);
+                } else {
+                  mapProgress.forEach(function (stats, nombre) {
+                    if (stats.mapName == elem.map) {
+                      if (elem.decision == "Win") stats.win += 1;else stats.lose += 1;
+                    }
+                  });
+                }
               }
             });
-
-            if (!mapIsCounted) {
-              recencedMap.push(elem.map);
-              that.pushFirstMap(mapProgress, elem.decision, elem.map);
-            } else {
-              mapProgress.forEach(function (stats, nombre) {
-                if (stats.mapName == elem.map) {
-                  if (elem.decision == "Win") stats.win += 1;else stats.lose += 1;
-                }
-              });
-            }
-          }
-        });
-        mapProgress.forEach(function (stats, nombre) {
-          stats.ratio = Math.round(eval(stats.win / stats.lose) * 100) / 100;
-        });
-        mapProgress = that.sortByKey(mapProgress, "ratio");
-        mapProgress.forEach(function (stats, nombre) {
-          document.querySelector(".mapAnalyse").innerHTML += "<div><span>" + stats.mapName + "</span><span>" + stats.ratio + "</span></div>";
+            mapProgress.forEach(function (stats, nombre) {
+              stats.ratio = Math.round(eval(stats.win / stats.lose) * 100) / 100;
+            });
+            mapProgress = that.sortByKey(mapProgress, "ratio");
+            mapProgress.forEach(function (stats, nombre) {
+              document.querySelector(".mapAnalyse").innerHTML += "<div><span>" + stats.mapName + "</span><span>" + stats.ratio + "</span></div>";
+            });
+          });
         });
       });
     });
   },
-  bindEvent: function bindEvent() {},
-  initComponents: function initComponents() {},
+  initComponents: function initComponents() {
+    this.getToken();
+  },
   casttimestampToDate: function casttimestampToDate(timeStamp) {
     var date = new Date(timeStamp * 1000);
     var day = date.getUTCDate();
